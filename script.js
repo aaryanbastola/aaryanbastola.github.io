26115158132cfb4907013a43f70b0c0c864c9bd9
Aaryan Bastola <aaryanbastola221@gmail.com>
2025-07-30 19:40:01 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// Smooth scroll for anchor links
-document.querySelectorAll('a.nav-link').forEach(function(link) {
-  link.addEventListener('click', function(e) {
-    const targetId = this.getAttribute('href').replace('#', '');
-    const targetEl = document.getElementById(targetId);
-    if (targetEl) {
-      e.preventDefault();
-      targetEl.scrollIntoView({ behavior: 'smooth' });
-    }
-  });
-});
+2b35563464c7635a1729b5d3f9a505008ac3cf78
